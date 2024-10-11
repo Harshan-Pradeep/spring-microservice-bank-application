b3e41fb5cc52eebb0d6d07c6e851c1f97394cae6
Harshan Pradeep <harshan.pradeep@outlook.com>
2024-10-11 11:40:11 +0530

    Implemented CircuitBraker to accounts microservices with resilience4j.

---
+++ accounts/src/main/java/com/harshan/accounts/service/client/CardsFallback.java
@@ -0,0 +1,15 @@
+package com.harshan.accounts.service.client;
+
+import com.harshan.accounts.dto.CardsDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardsFallback implements CardsFeignClient{
+
+    @Override
+    public ResponseEntity<CardsDto> fetchCardDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+
+}
